d961a3e277cbac0959f34047879fce92ecbbe544
v-kuntsevich <kwakin@gmail.com>
2025-05-12 22:36:18 +0300

    fix: debug webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-  entry: "./src/app.ts",
+  entry: "./cluster/cluster.ts",
   target: "node",
   mode: "development",
   module: {
